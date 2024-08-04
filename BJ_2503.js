1c148d66a2f189da15eddf3815d4b9c54ebca79d
Minju <lmj042398@gmail.com>
2024-08-04 18:37:11 +0900

    백준 숫자야구

--- BJ_2503.js
+++ BJ_2503.js
@@ -1,36 +1,73 @@
-const input = require('fs').readFileSync('input_2503.txt').toString().split('\n')
+// const input = require('fs').readFileSync('input_2503d.txt').toString().split('\n')
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n')
 const N = input[0]
 
 let num = []
-let st = []
-let ball = []
-let answer = 0;
+
+let ans = [];
+let visit = Array(10).fill(0)
 for(let i=1; i<=N; i++){
     let arr=input[i].split(' ').map(Number)
-    num.push(arr[0])
-    st.push(arr[1])
-    ball.push(arr[2])
+    num.push(arr)
 }
 
-console.log(num, st, ball)
+function isBall(answer, num){
+    let cnt = 0
+    for(let i=0; i<3; i++){
+        for(let j=0; j<3; j++){
+            if(i !== j){
+                if(answer[i] === num[j]) cnt++
+            }
+        }
+    }
+    return cnt
+}
 
-/**
-13x
-3x2
-x12
 
-3xx
-x5x
-xx6
+function isStrike(answer, num){
+    let cnt = 0;
+    for(let i=0; i<3; i++){
+        if(answer[i] === num[i]) cnt++
+    }
+    return cnt
+}
 
-32x
-3x7
-x27
+function dfs(res, visit){
+    if(res.length == 3){
+        let flag = true
+        let answer = res.join("")
 
-x4x
-xx4
-8xx
-xx8
-9xx
-x9x
- */
+        for(const [n, s, b] of num){
+            const strN = n.toString()
+            const strike = isStrike(answer, strN)
+
+            if(s !== strike){
+                flag = false
+                break
+            }
+
+            const ball = isBall(answer, strN)
+            if(b !== ball){
+                flag = false
+                break
+            }
+        }
+
+        if(flag) ans.push(answer)
+        return
+    }
+
+    for(let i=1; i<=9; i++){
+        if(!visit[i]){
+            visit[i] = 1
+            res.push(i)
+            dfs(res, visit)
+            visit[i] = 0
+            res.pop()
+        }
+    }
+
+}
+
+dfs([], visit)
+console.log(ans.length)
