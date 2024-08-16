ca57d2b519c37ee40c1340101f32f1a225ef0d84
minju0423 <125239229+minju0423@users.noreply.github.com>
2024-08-16 11:29:04 +0900

    1544_수정

--- BJ_1544.js
+++ BJ_1544.js
@@ -1,31 +1,28 @@
 const input = require('fs').readFileSync('input_1544.txt').toString().trim().split('\n')
 
-const N = +input.shift()
+let N = +input.shift()
 let arr = input.map(el=> el.trim())
 
-let answer = new Set()
 
-let i=0
-while(i<N){
-    let str = arr[i]
-    let reWord = str.concat("",str)
+
+for(let i=0; i<N; i++){
+    let prevStr = arr[i]
+    for(let j=i+1; j<N; j++){
+        let str = arr[j]
+        
+        if(str.length == prevStr.length){//현재 단어와 전 단어의 길이가 같으면
+            let strConcat = str.concat("", str) //현재단어 두개 붙임
+
+            if(strConcat.includes(prevStr)){// 그 전 단어가 포함되어 있으면
+                //arr 배열안에 있는 것을 지움
+                arr.splice(j,1)
+                N--
+                j--
+            }
+        }
+    }
     
-    let notInclude = arr.filter((el)=>reWord.includes(el))
-    console.log(notInclude)
-    answer.add(notInclude)
-
-    i++
+    
 }
+console.log(N)
 
-
-console.log(answer)
-
-/**
- * 1. 단어가 있는 배열 돌기
- *      1) idx = 1부터 시작 0번째 배열은 비교 대상 
- * 2. 단어의 문자 하나씩 돌기
- *      1) 그 전의 단어와 현재 단어의 길이가 같으면 비교, 아니면 answer++
- * 3. 현재 단어가 
- * 
- * atsats
- */
